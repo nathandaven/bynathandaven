4e26539e0f017164edc81f16b3b1bfb079c5995e
nathandaven <nathandaven@gmail.com>
2024-09-14 12:56:16 -0400

    cleanup 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Container from "@/app/_components/container";
 import { HeroPost } from "@/app/_components/hero-post";
-import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
 import { GoogleAnalytics } from "@next/third-parties/google";
