80a89b331c8900181df403ef0961c24bead906a4
nathandaven <nathandaven@gmail.com>
2025-08-10 12:03:07 -0400

    bugfix

--- src/app/_svg/code.tsx
+++ src/app/_svg/code.tsx
@@ -9,17 +9,13 @@ type PropsWithChildren<P = unknown> = P & {
 
 const Code: FunctionComponent<PropsWithChildren> = ({ children, className }) => (
   <svg
-    className={classNames("stroke-dark-primary stroke-2 dark:stroke-light-primary", className)}
+    className={classNames("stroke-light-primary stroke-2", className)}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
   >
-    <path
-      d="M7 8L3 11.6923L7 16M17 8L21 11.6923L17 16M14 4L10 20"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
+    <title>Code</title>
+    <path d="M7 8L3 11.6923L7 16M17 8L21 11.6923L17 16M14 4L10 20" />
   </svg>
 );
 export default Code;
